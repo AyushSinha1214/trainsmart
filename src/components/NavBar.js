eb0ec345fdbe74ef12db1e6d1809068df1e3f6cc
AyushSinha1214 <ayushsinha1214@gmail.com>
2025-04-10 17:55:25 +0530

    day 2 updates

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {link} from  'react-router-dom';
+import {Stack} from '@mui/material';
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
