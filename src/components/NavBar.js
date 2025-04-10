7b16b52fca79eda7ec3c05db70169c4b1ad29895
AyushSinha1214 <ayushsinha1214@gmail.com>
2025-04-10 23:33:46 +0530

    day 2 updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import {link} from  'react-router-dom';
-import {Stack} from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Stack>
+      <Link to="/">
+        <img src={Logo} alt='logo' style={{
+          width: '48px', height: '48px', margin: '0 20px'
+        }} />
+      </Link>
+    </Stack>
   )
 }
 
