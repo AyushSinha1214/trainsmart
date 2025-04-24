e0a6a7a95d48c2f4f1b2bed5441ccbf475b3fa2a
AyushSinha1214 <ayushsinha1214@gmail.com>
2025-04-24 12:45:39 +0530

    day 10

--- src/App.js
+++ src/App.js
@@ -5,28 +5,18 @@ import { Box } from '@mui/material';
 import './App.css';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
-import NavBar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { BrowserRouter } from 'react-router-dom';
 
-<BrowserRouter
-  future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
->
-  {/* your routes here */}
-</BrowserRouter>
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-
-const App = () => {
-  return (
-    <Box width="400px" sx={{ width: {xl: '1488px'}}} m="auto">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/excercise/:id" element={<ExcerciseDetail />} />
-      </Routes>
-      <Footer />
-    </Box>
-  )
-}
-
-export default App
+export default App;
